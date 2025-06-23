cc466f4be934264a67ab8eaf33bd9a9ebf4a7b3c
ganggun <165283835+ganggun@users.noreply.github.com>
2025-06-23 11:29:58 +0900

    add

---
+++ src/test/java/com/ganggun/petition94/Petition94ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ganggun.petition94;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Petition94ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
